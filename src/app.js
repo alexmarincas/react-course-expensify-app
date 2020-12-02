383fad9205549a54ba7f3e589d81f94a044b12d2
Alexandru Marincas <alexandru.marincas@yahoo.ro>
2020-12-02 02:38:40 +0200

    Comment out dummy expense data

--- src/app.js
+++ src/app.js
@@ -15,13 +15,13 @@ import './styles/styles.scss';
 
 const store = configureStore(); // variabila asta este folosita in Provider
 
-store.dispatch(addExpense({description : 'Water bill', amount: 4500}));
-store.dispatch(addExpense({description : 'Gas bill', createdAt: 3504}));
-store.dispatch(addExpense({description : 'Rent', amount: 109500}));
+// store.dispatch(addExpense({description : 'Water bill', amount: 4500}));
+// store.dispatch(addExpense({description : 'Gas bill', createdAt: 3504}));
+// store.dispatch(addExpense({description : 'Rent', amount: 109500}));
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log( visibleExpenses );
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log( visibleExpenses );
 
 const jsx = (
     <Provider store={store}>
